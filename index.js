63d8564c513b6f6f374136a86d40b95c2e75c346
sazawa <itsrayansazawa@gmail.com>
2025-07-02 12:29:38 +0545

    fix mmp

--- index.js
+++ index.js
@@ -80,68 +80,40 @@ app.post('/deadline', async  (req, res) => {
     }
 });
 
-
-const checkpointToKey = {
-    [1]: "https://lootdest.org/s?RwLz3rc7",
-    [2]: "https://lootdest.org/s?VcnBoO9V", 
-    [3]: "https://lootdest.org/s?rHV5QjUP",
-}
-
 //-----MMP KEY-------//
 app.get('/mmp', async (req,res) => {
     res.sendFile(path.join(viewsPath, 'mmpKey.html'));
 })
 
-app.post('/mmp/getcheckpoint', async (req,res)=> {
+app.post('/mmp', async (req,res)=> {
+    const token = req.body.token;
+    const url = "https://work.ink/_api/v2/token/isValid/" + token + "?deleteToken=1";
+    const response = await fetch(url)
+    const resBody = await response.json()
+    const isValid = resBody.valid
     
-
-    let userCheckpoint = 1;
-    if (req.cookies.checkpoint) {
-        userCheckpoint = parseInt(req.cookies.checkpoint);
-    }
-    res.json({checkpoint:userCheckpoint})
-})
-
-app.post('/mmp/geturl', async (req, res) => {
-
-    let userCheckpoint = 1;
-    if (req.cookies.checkpoint) {
-        userCheckpoint = parseInt(req.cookies.checkpoint);
-    }
-    if (userCheckpoint != 1 && req.body.referrer != "https://lootdest.org/") {
-        res.clearCookie("checkpoint", {httpOnly:true});
-        res.json({url:"",key:""})
+    if (!isValid) {
+        res.json({key:""})
         return;
     }
-    if (userCheckpoint == 3) {
-        res.clearCookie("checkpoint", {httpOnly:true});
 
-        try {
-            const response = await fetch('https://betterkeysystem.sazawa.workers.dev/?key=IMGENERATINGANEWKEYRAHHHHHHHHHHHHHHH&type=MMPFREE');
-        
-            if (!response.ok) {
-              const errorText = await response.text();
-              throw new Error(`HTTP error ${response.status}: ${errorText}`);
-            }
-        
-            const responseText = await response.text();
-            res.json({key:responseText})
+    try {
+        const response = await fetch('https://betterkeysystem.sazawa.workers.dev/?key=IMGENERATINGANEWKEYRAHHHHHHHHHHHHHHH&type=MMPFREE');
     
-        } catch (error) {
-            console.error("Error getting key:", error);
-            res.status(500).json({key:"Error getting key"});
+        if (!response.ok) {
+          const errorText = await response.text();
+          throw new Error(`HTTP error ${response.status}: ${errorText}`);
         }
-        return;
+    
+        const responseText = await response.text();
+        res.json({key:responseText})
+
+    } catch (error) {
+        console.error("Error getting key:", error);
+        res.status(500).json({key:"Error getting key"});
     }
-    userCheckpoint += 1;
-    res.cookie("checkpoint", userCheckpoint, {maxAge: 1000 * 60 * 10, httpOnly: true});
-    const checkPoinToSend = checkpointToKey[userCheckpoint-1]
-    res.json({url:checkPoinToSend});
 })
 
-app.post('/mmp/leave', async(req,res) => {
-    res.clearCookie("checkpoint", {httpOnly:true});
-})
 
 
 
